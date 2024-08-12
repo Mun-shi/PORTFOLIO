0fec4dfced7f6383de23991e4e5ade74b2eae0eb
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2024-08-12 20:26:47 +0530

    update services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,29 +1,56 @@
 import "./services.scss";
-import {motion} from "framer-motion";
+import {motion,useInView} from "framer-motion";
+import { useRef } from "react";
+
+const variants = {
+    initial:{
+        x:-500,
+        y:100,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        y:0,
+        opacity:1,
+        transition:{
+            duration:1,
+            staggerChildren:0.1,
+        }
+    }
+}
 
 const Services = () => {
+
+  const ref = useRef();
+
+  const isInView = useInView(ref,{margin:"-100px"});
   return (
-    <motion.div className="services">
-        <motion.div className="textContainer">
+    <motion.div className="services"
+     variants={variants}
+     initial="initial" 
+     ref={ref}
+     animate={isInView ? "animate" : "initial"}
+     >
+        <motion.div className="textContainer" variants={variants}>
             <p>I focus on helping your brand grow
                 <br />and move forward</p>
             <hr/>
         </motion.div> 
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer" variants={variants}>
             <div className="title">
                 <img src="/people.webp" alt="" />
                 <h1>
-                    <b>Unique</b> Ideas
+                    <motion.b whileHover={{color:"orange"}}>Unique</motion.b> Ideas
                 </h1>
             </div>
             <div className="title">
                 <h1>
-                    <b>For Your</b> Business
+                    <motion.b whileHover={{color:"orange"}}>For Your</motion.b> Business
                 </h1>
                 <button>WHAT WE DO?</button>
             </div>
         </motion.div>
-        <motion.div className="listContainer">
+        <motion.div className="listContainer" variants={variants}>
             <motion.div className="box"
             whileHover={{background: "lightgray", color: "black"}}>
                 <h2>Branding</h2>
