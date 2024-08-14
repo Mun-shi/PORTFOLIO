3ca73bb86ef4035f8cb2eba3196a3026ff521266
Mun-shi <90496225+Mun-shi@users.noreply.github.com>
2024-08-14 14:42:11 +0530

    update portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,36 +1,73 @@
 import "./portfolio.scss";
+import {motion,useScroll,useSpring, useTransform} from "framer-motion";
+import { useRef } from "react";
 
 const items = [
     {
         id:1,
-        title:"Dijkstra’s Algorithm Visualizer",
-        img:"https://www.pexels.com/photo/man-fishing-on-seashore-with-dog-22708188/",
+        title:"Traffic Sign Recognition System",
+        img:"Traffic.jpg",
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id:2,
-        title:"Traffic Sign Recognition System",
-        img:"https://www.pexels.com/photo/man-fishing-on-seashore-with-dog-22708188/",
+        title:"Dijkstra’s Algorithm Visualizer",
+        img:"digis.png",
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
         id:3,
         title:"Library Management System",
-        img:"https://www.pexels.com/photo/man-fishing-on-seashore-with-dog-22708188/",
+        img:"library.jpg",
         desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
 ]
 
 const Single = ({item}) =>{
+    const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target:ref,
+    });
+
+    const y = useTransform(scrollYProgress, [0,1],[-300, 300])
+
     return(
-        <section>
-            {item.title}
+        <section ref={ref}>
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer">
+                <img src={item.img} alt="" />
+                </div>
+                <motion.div className="textContainer" style={{y}}>
+                    <h2>{item.title}</h2>
+                    <p>{item.desc}</p>
+                    <button>See Demo</button>
+                </motion.div>
+                </div>
+            </div>
         </section>
     )
 }
 const Portfolio = () => {
+    const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target:ref,
+        offset:["end end","start start"],
+    });
+
+    const scaleX = useSpring(scrollYProgress,{
+        stiffness:100,
+        damping:30,
+    });
   return (
-    <div className="portfolio">
+    <div className="portfolio" ref={ref}>
+        <div className="progress">
+            <h1>Featured works</h1>
+            <motion.div style={{scaleX:scaleX}} className="progressBar">
+            </motion.div>
+        </div>
       {items.map((item) =>(
         <Single item={item} key={item.id}/>
       ))}
